c68801cd717e20b25ddb1b92c50babf2d480e9ce
Abdullah1234-lab-skech <abdullahhussainn8847@gmail.com>
2025-10-07 20:24:33 +0300

    activity

---
+++ country.py
@@ -0,0 +1,15 @@
+country_codes = {'Pakistan': '+92',
+                    'India': '+91',
+                    'Bangladesh': '+880',
+                    'Sri Lanka': '+94',
+                    'Russia': '+7',} 
+print('Country code for Pakistan -') 
+print(country_codes.get('Pakistan','Not found')) 
+print('Country code for India -')  
+print(country_codes.get('India','Not found')) 
+print('Country code for Bangladesh - ')
+print(country_codes.get('Bangladesh','Not found'))
+print('Country code for Sri Lanka - ')
+print(country_codes.get('Sri Lanka','Not found'))   
+print('Country code for Russia - ')
+print(country_codes.get('Russia','Not found'))
